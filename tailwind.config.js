871d27949d57804298ead30b8b27ca34b3cee22a
jplej <jplr8922@gmail.com>
2024-11-05 14:33:58 -0500

    ok ready to deploy on netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,43 @@ module.exports = {
     './pages/**/*.html',
   ],
   theme: {
-    theme: {
-      extend: {},
+    extend: {
+      colors: {
+        // Background Colors for Dark UI Elements (Interactive)
+        BgDark: {
+          primary: '#3B4252',   // `nord1`
+          secondary: '#434C5E',  // `nord2`
+          border: '#4C566A',     // `nord3`
+        },
+
+        // Background Colors for Light Non-Interactive Elements
+        BgLight: {
+          primary: '#ECEFF4',  // `nord6`
+          secondary: '#E5E9F0', // `nord5`
+        },
+
+        // Text Colors on Dark Backgrounds
+        textOnDark: {
+          primary: '#ECEFF4',  // `nord6`
+          secondary: '#E5E9F0', // `nord5`
+        },
+
+        // Text Colors on Light Backgrounds
+        textOnLight: {
+          primary: '#2E3440',  // `nord0`
+          secondary: '#4C566A', // `nord3`
+        },
+
+        // Accent Colors for Highlights and Actions
+        accent: {
+          main: '#8FBCBB',   // `nord7`
+          highlight: '#88C0D0',        // `nord8`
+          error: '#BF616A',        // `nord11` (red)
+          warning: '#EBCB8B',      // `nord13` (yellow)
+          success: '#A3BE8C',      // `nord14` (green)
+          url: '#b48ead' // `nord15' (purple)
+        },
+      },
     },
   },
   plugins: []
